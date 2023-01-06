5681ff979e7d8c6761a23712252e915f8b4bc2a9
mdchad <irsyad.muhd@gmail.com>
2023-01-06 21:22:56 +0800

    remove exit

--- components/GridItemSettings.tsx
+++ components/GridItemSettings.tsx
@@ -110,7 +110,6 @@ function GridLayoutSettings({
               transition={{ duration: 0.3 }}
               initial={{ opacity: 0.5, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 10 }}
             >
               <span>
                 {itemIndex.toString().length === 1
